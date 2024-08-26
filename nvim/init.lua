20a3dfec2181f8c1b5f7855fba89d11784a8e545
Max <caradhras11@gmail.com>
2024-08-26 04:25:05 +0200

    next

--- nvim/init.lua
+++ nvim/init.lua
@@ -49,6 +49,18 @@ vim.filetype.add({
 
 
 
+-------------------------------------------------------------------------------
+-- Autocmds
+-------------------------------------------------------------------------------
+
+--auto save
+vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost" }, {
+    callback = function()
+        if vim.bo.modified and not vim.bo.readonly and vim.fn.expand("%") ~= "" and vim.bo.buftype == "" then
+            vim.api.nvim_command('silent update')
+        end
+    end,
+})
 
 -------------------------------------------------------------------------------
 -- Bootstrap Package Manager
