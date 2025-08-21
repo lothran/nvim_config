8310b24722daba03fd15949b3c300d86bde24026
jloth <jacob.loth@solectrix.de>
2025-08-21 15:32:56 +0200

    next

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,17 +12,18 @@ vim.opt.cmdheight   = 0
 vim.opt.numberwidth = 4
 vim.opt.signcolumn  = 'yes'
 vim.opt.clipboard   = 'unnamedplus'
-vim.g.clipboard     = {
+vim.g.clipboard = {
   name = 'OSC 52',
   copy = {
-    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
-    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+    ['+'] = require('vim.ui.clipboard.osc52').copy '+',
+    ['*'] = require('vim.ui.clipboard.osc52').copy '*',
   },
   paste = {
-    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
-    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+    ['+'] = require('vim.ui.clipboard.osc52').paste '+',
+    ['*'] = require('vim.ui.clipboard.osc52').paste '*',
   },
 }
+
 
 
 vim.o.sessionoptions  = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
