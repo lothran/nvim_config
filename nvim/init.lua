582dae1b3d159c466ba1e809b1c4a801bbc5c646
jloth <jacob.loth@solectrix.de>
2026-02-18 16:04:43 +0100

    install

--- nvim/init.lua
+++ nvim/init.lua
@@ -37,25 +37,6 @@ vim.filetype.add({
 -------------------------------------------------------------------------------
 
 
-vim.keymap.set({ 'n', 'i', 't', 'v' }, "<C-h>", "<C-w>h")
-vim.keymap.set({ 'n', 'i', 't', 'v' }, "<C-j>", "<C-w>j")
-vim.keymap.set({ 'n', 'i', 't', 'v' }, "<C-k>", "<C-w>k")
-vim.keymap.set({ 'n', 'i', 't', 'v' }, "<C-l>", "<C-w>l")
-vim.keymap.set('n', "<space>sh", "<CMD>LspClangdSwitchSourceHeader<CR>")
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
-vim.keymap.set({ 'n', 'i', 't', 'v' }, '<C-1>', '<CMD>1ToggleTerm<CR>')
-vim.keymap.set({ 'n', 'i', 't', 'v' }, '<C-2>', '<CMD>2ToggleTerm<CR>')
-vim.keymap.set({ 'n', 'i', 't', 'v' }, '<C-3>', '<CMD>3ToggleTerm<CR>')
-vim.keymap.set({ 'n', 'i', 't', 'v' }, '<C-4>', '<CMD>4ToggleTerm<CR>')
-
-vim.keymap.set({ 'n', 'i', 'v' }, '<C-n>', '<C-d>')
-vim.keymap.set({ 'n', 'i', 'v' }, '<C-p>', '<C-u>')
-
-vim.keymap.set('t', '<esc><esc>', [[<C-\><C-n>]])
-vim.keymap.set({ 'n', 'i', 't', 'v' }, '<c-z>', function() end);
 
 
 
@@ -69,6 +50,14 @@ vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost" }, {
     if vim.bo.modified and not vim.bo.readonly and vim.fn.expand("%") ~= "" and vim.bo.buftype == "" then
       vim.api.nvim_command('silent update')
     end
+  end,
+})
+vim.o.title = true
+local bin = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+  callback = function()
+      vim.o.titlestring = bin
   end,
 })
 
@@ -569,107 +558,6 @@ local plugins = {
     event = "BufRead",
     branch = "main",
     build = ":TSUpdate",
-    ---@class TSConfig
-    opts = {
-      -- custom handling of parsers
-      ensure_installed = {
-        "astro",
-        "bash",
-        "c",
-        "css",
-        "diff",
-        "go",
-        "gomod",
-        "gowork",
-        "gosum",
-        "graphql",
-        "html",
-        "javascript",
-        "jsdoc",
-        "json",
-        "json5",
-        "lua",
-        "luadoc",
-        "luap",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "query",
-        "regex",
-        "toml",
-        "tsx",
-        "typescript",
-        "vim",
-        "vimdoc",
-        "yaml",
-        "ruby",
-      },
-    },
-    config = function(_, opts)
-      -- install parsers from custom opts.ensure_installed
-      if opts.ensure_installed and #opts.ensure_installed > 0 then
-        require("nvim-treesitter").install(opts.ensure_installed)
-        -- register and start parsers for filetypes
-        for _, parser in ipairs(opts.ensure_installed) do
-          local filetypes = parser -- In this case, parser is the filetype/language name
-          vim.treesitter.language.register(parser, filetypes)
-
-          vim.api.nvim_create_autocmd({ "FileType" }, {
-            pattern = filetypes,
-            callback = function(event)
-              vim.treesitter.start(event.buf, parser)
-            end,
-          })
-        end
-      end
-
-      -- Auto-install and start parsers for any buffer
-      vim.api.nvim_create_autocmd({ "BufRead" }, {
-        callback = function(event)
-          local bufnr = event.buf
-          local filetype = vim.api.nvim_get_option_value("filetype", { buf = bufnr })
-
-          -- Skip if no filetype
-          if filetype == "" then
-            return
-          end
-
-          -- Check if this filetype is already handled by explicit opts.ensure_installed config
-          for _, filetypes in pairs(opts.ensure_installed) do
-            local ft_table = type(filetypes) == "table" and filetypes or { filetypes }
-            if vim.tbl_contains(ft_table, filetype) then
-              return -- Already handled above
-            end
-          end
-
-          -- Get parser name based on filetype
-          local parser_name = vim.treesitter.language.get_lang(filetype) -- might return filetype (not helpful)
-          if not parser_name then
-            return
-          end
-          -- Try to get existing parser (helpful check if filetype was returned above)
-          local parser_configs = require("nvim-treesitter.parsers")
-          if not parser_configs[parser_name] then
-            return -- Parser not available, skip silently
-          end
-
-          local parser_installed = pcall(vim.treesitter.get_parser, bufnr, parser_name)
-
-          if not parser_installed then
-            -- If not installed, install parser synchronously
-            require("nvim-treesitter").install({ parser_name }):wait(30000)
-          end
-
-          -- let's check again
-          parser_installed = pcall(vim.treesitter.get_parser, bufnr, parser_name)
-
-          if parser_installed then
-            -- Start treesitter for this buffer
-            vim.treesitter.start(bufnr, parser_name)
-          end
-        end,
-      })
-    end,
   },
 
   {
@@ -747,7 +635,7 @@ local plugins = {
   },
   {
     'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suitemini
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
     ---@module 'render-markdown'
@@ -832,6 +720,12 @@ local plugins = {
       }
     },
   },
+  { 
+    'nvim-mini/mini.nvim', version = false,
+    config=function() 
+        require("mini.git").setup({})
+    end
+  },
   {
     "bassamsdata/namu.nvim",
     config = function()
@@ -906,5 +800,25 @@ local plugins = {
 }
 
 require('lazy').setup(plugins)
+
+vim.keymap.set({ 'n', 'i', 't', 'v' }, "<C-h>", "<C-w>h")
+vim.keymap.set({ 'n', 'i', 't', 'v' }, "<C-j>", "<C-w>j")
+vim.keymap.set({ 'n', 'i', 't', 'v' }, "<C-k>", "<C-w>k")
+vim.keymap.set({ 'n', 'i', 't', 'v' }, "<C-l>", "<C-w>l")
+vim.keymap.set('n', "<space>sh", "<CMD>LspClangdSwitchSourceHeader<CR>")
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+vim.keymap.set({ 'n', 'i', 't', 'v' }, '<C-1>', '<CMD>1ToggleTerm<CR>')
+vim.keymap.set({ 'n', 'i', 't', 'v' }, '<C-2>', '<CMD>2ToggleTerm<CR>')
+vim.keymap.set({ 'n', 'i', 't', 'v' }, '<C-3>', '<CMD>3ToggleTerm<CR>')
+vim.keymap.set({ 'n', 'i', 't', 'v' }, '<C-4>', '<CMD>4ToggleTerm<CR>')
+
+vim.keymap.set({ 'n', 'i', 'v' }, '<C-n>', '<C-d>')
+vim.keymap.set({ 'n', 'i', 'v' }, '<C-p>', '<C-u>')
+
+vim.keymap.set('t', '<esc><esc>', [[<C-\><C-n>]])
+vim.keymap.set({ 'n', 'i', 't', 'v' }, '<c-z>', function() end);
 
 vim.cmd [[colorscheme  gruvbox]]
